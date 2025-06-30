87df7d1f7a15dc49ce10c9cc747bed7f7a3daad5
Prasinos <np3106@nyumc.org>
2025-06-30 19:07:47 -0400

    edited sbatch script

--- run_segmentation.sh
+++ run_segmentation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=eeg_seg              # minimal job name
 #SBATCH --partition=cpu_medium          # Big Purple CPU queue
-#SBATCH --time=00:02:00                  # wall-clock limit (hh:mm:ss)
+#SBATCH --time=01:00:00                  # wall-clock limit (hh:mm:ss)
 #SBATCH --cpus-per-task=1              # threads for Python / MNE
 #SBATCH --mem=64G                      # RAM for the job
 #SBATCH --chdir=/gpfs/scratch/np3106/eeg_cleaning   # work dir = input/output root
@@ -9,8 +9,8 @@
 
 # ---- load your Python environment (edit to match your setup) ----
 module purge
-module load anaconda3/cpu/2022.10            # or any module that provides Conda
-conda activate /gpfs/scratch/np3106/conda-envs/eeg-clean                # env with mne, numpy, etc.
+module load anaconda3/gpu/new            # or any module that provides Conda
+conda activate /gpfs/scratch/np3106/venvs/eeg-clean                # env with mne, numpy, etc.
 
 # ---- run the script ------------------------------------------------
 python data_segmentation.py
